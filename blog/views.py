fc1a239a0eb2ff442d977a72196156e16ee4a66a
Mihail-sudo <321mig123@gmail.com>
2025-04-19 16:21:11 +0500

    homework

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.views import generic
 from .models import Post
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,12 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
